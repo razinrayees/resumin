0ef5959342bc189c3329fa078d2c4b362adbb071
Razin Rayees <razin.rayeesk@gmail.com>
2025-07-23 20:16:50 +0530

    login fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -540,6 +540,7 @@ export const Login: React.FC<LoginProps> = ({ isSignupMode = false }) => {
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-200" />
                 </div>
+                <div className="relative flex justify-center text-sm">
                   <span className="px-4 bg-white text-gray-500">Or continue with</span>
                 </div>
               </div>
